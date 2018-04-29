846c87eb030fe71713a6a7b3e32b376bc421a29a
viktor gopanchuk <vitugan@gmail.com>
2018-04-29 10:27:14 +0300

    Task 4 hw6 complete

--- hw-tasks/hw6/task4/index.js
+++ hw-tasks/hw6/task4/index.js
@@ -1,36 +1,51 @@
 var applyLanguage = function (lang) {
 	alert('now language is: ' + lang);
+    var langEls = document.getElementsByClassName('lang');    
+    for (var i=0; i<langEls.length; i++) {        
+        var langEl = langEls[i];        
+        if(langEl.classList.contains('lang-' + lang)){
+            langEl.style.display = 'inline';
+            langEl.classList.add('visible');
+        } else {
+            langEl.style.display = 'none';
+            langEl.classList.remove('visible');
+        }
+    }
+}
+
+var setLanguageInput = function(lang){
+    var inputEls = document.querySelectorAll("[name='lang']");
+    for(inputEl of inputEls){
+        if(inputEl.value === lang){
+            inputEl.checked = true;
+        } else {
+            inputEl.checked = false;
+        }
+    }    
 }
 
 var getCurrentLanguage = function () {
     var defaultLanguage = 'ua';
-    // return window.location.hash.substring(1) || defaultLanguage;
-    return defaultLanguage;    
+    return window.location.hash.substring(1) || defaultLanguage;    
 }
 
 var currentLang = getCurrentLanguage();
 var langEls = document.getElementsByClassName('lang-' + currentLang);
 
-for (var i=0; i<langEls.length; i++) {
-	var langEl = langEls[i];
-	
-  langEl.style.display = 'inline';
-  langEl.classList.add('visible');
-}
+applyLanguage(currentLang);
+setLanguageInput(currentLang);
 
 // $<prefix> == DOMElement
 var $save = document.getElementById('save');
 
-$save.addEventListener('click', function(){
-    // var lang = document.getElementById('languages');
+$save.addEventListener('click', function(){    
     var lang = document.getElementsByName('lang');
-    // window.location.hash = lang.value;
+    
     for(var i = 0; i < lang.length; i++){
-        if(lang[i].checked === true){
-            var lang = lang[i].value;
+        if(lang[i].checked === true){            
+            var lang = lang[i].value;                                   
+            applyLanguage(lang);
             window.location.hash = lang;
-            console.log(lang)
         }
     }
-    console.log(lang);
 });
