fb0b37c1645f0b9f43a39dd0ee5eb7e1aa1b11eb
viktor gopanchuk <vitugan@gmail.com>
2018-05-01 15:23:46 +0300

    Update links for hw6 task5

--- hw-tasks/hw6/task5/script.js
+++ hw-tasks/hw6/task5/script.js
@@ -4,10 +4,8 @@ var box3 = document.getElementById('box3');
 var box11 = document.getElementById('box11');
 var box22 = document.getElementById('box22');
 var box33 = document.getElementById('box33');
-var a = document.getElementById('preventDefault');
 
-var onClickPrevent = function (e) {  
-  var $el = e.target;    
+var onClickPrevent = function (e) {    
   e.preventDefault();
   changeColor(this);
 }
