cf2f4866067a052b45a633835623d71986eab0bf
viktor <vitugan@gmail.com>
2018-05-03 08:39:37 +0300

    Started Casino task

--- hw-tasks/hw7/casino/app.js
+++ hw-tasks/hw7/casino/app.js
@@ -1,3 +1,36 @@
-function Casino(){
-    
+function Casino(options){
+    this.machinesCount = options.machinesCount;
+    this.totalCash = options.totalCash;
+    var casinoMachines = [];
+    if(this.machinesCount>0){
+        for(var i =0; i<machinesCount; i++){
+            casinoManhines[] = CreateSlotMachine(cash);
+        }
+    }
+
+    function CreateSlotMachine(options){
+        var machineCash = this.totalCash / this.machinesCount;
+        return new SlotMachine(options);
+    }
 }
+Casino.prototype.getTotalSum = function(){
+    return this.total_cash;
+}
+Casino.prototype.getMachinesCount = function(){
+    var machines;
+    return machines;
+}
+Casino.prototype.addMachine = function(){}
+
+Casino.prototype.removeMachine = function(){}
+
+Casino.prototype.cashOut = function(){}
+
+function SlotMachine(options){
+    this.cash = options.cash;
+    this.luckybox = options.luckybox;
+}
+SlotMachine.prototype.getTotalSum = function(){}
+SlotMachine.prototype.cashOut = function(){}
+SlotMachine.prototype.cashIn = function(){}
+SlotMachine.prototype.letsPlay = function(){}
